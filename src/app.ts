885190b8de5c6514981c5ce953b36743b6d983e0
Bappy <bappymahmud321@gmail.com>
2023-11-23 11:01:00 +0600

    add eslint-prettier

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
-import express, { Application, Request, Response } from 'express'
-import cors from 'cors'
-const app : Application = express()
-const port = 3000
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+const app: Application = express();
 
-app.use(express.json)
-app.use(cors())
-app.get('/', (req : Request, res : Response) => {
-  res.send('Hello World!')
-})
+app.use(express.json);
+app.use(cors());
+app.get('/', (req: Request, res: Response) => {
+  const a = 10;
+  res.send(a);
+});
 
 console.log(process.cwd());
-export default app
+export default app;
